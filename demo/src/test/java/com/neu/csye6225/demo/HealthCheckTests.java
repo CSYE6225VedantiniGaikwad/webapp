fc641670d6519d6341df82be2c867a16188696c5
Vedantini Gaikwad <114186433+VedantiniG@users.noreply.github.com>
2023-10-09 14:24:42 -0400

    Update HealthCheckTests.java

--- demo/src/test/java/com/neu/csye6225/demo/HealthCheckTests.java
+++ demo/src/test/java/com/neu/csye6225/demo/HealthCheckTests.java
@@ -23,6 +23,6 @@ public class HealthCheckTests {
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
 
         // Assert that the response status code is 200 OK
-        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertEquals(204, responseEntity.getStatusCodeValue());
     }
 }
