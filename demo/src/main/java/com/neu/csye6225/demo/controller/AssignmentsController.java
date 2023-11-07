7fea32c9661ddb40d2e8a5ae51882f503540a870
Vedantini Gaikwad <114186433+VedantiniG@users.noreply.github.com>
2023-11-07 16:13:13 -0500

    Adding statsd to application

--- demo/src/main/java/com/neu/csye6225/demo/controller/AssignmentsController.java
+++ demo/src/main/java/com/neu/csye6225/demo/controller/AssignmentsController.java
@@ -16,6 +16,9 @@ import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
+import com.timgroup.statsd.StatsDClient;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+
 @RestController
 @Slf4j
 public class AssignmentsController {
@@ -25,6 +28,8 @@ public class AssignmentsController {
     @Autowired
     private ValidationService validationService;
     private static final String SCHEMA_PATH = "static/schema.json";
+
+    private static final StatsDClient statsd = new NonBlockingStatsDClient("statsdClient", "dev.vedantinigaikwad.me", 8125);
 
     public AssignmentsController(AssignmentsService assignmentsService, ValidationService validationService) {
         this.assignmentsService = assignmentsService;
@@ -37,6 +42,7 @@ public class AssignmentsController {
             return ResponseEntity.status(400).build();
         }
         List<Assignments> assignmentsList = assignmentsService.getAllAssignments();
+        statsd.incrementCounter("api.assignments.getAll");
         return ResponseEntity.ok(assignmentsList);
     }
 
@@ -45,9 +51,11 @@ public class AssignmentsController {
         try {
             JsonNode jsonNodeRequest = validationService.jsonValidation(s, SCHEMA_PATH);
             assignmentsService.creatAssignments(jsonNodeRequest);
+            statsd.incrementCounter("api.assignments.create");
             return ResponseEntity.status(201).build();
         } catch (Exception e) {
             log.error(e.getMessage());
+            statsd.incrementCounter("api.assignments.create.failed");
             return ResponseEntity.status(400).build();
         }
     }
@@ -56,12 +64,14 @@ public class AssignmentsController {
     public ResponseEntity<Object> getAssignmentById ( @PathVariable String id ) {
         UUID uuid = UUID.fromString(id);
         Assignments assignments = assignmentsService.getAssignmentsById(uuid);
+        statsd.incrementCounter("api.assignments.getById");
         return ResponseEntity.status(200).body(assignments);
     }
 
     @DeleteMapping("/v1/assignments/{id}")
     public ResponseEntity<Object> deleteAssignmentById ( @PathVariable String id ) {
         UUID uuid = UUID.fromString(id);
+        statsd.incrementCounter("api.assignments.delete");
         return assignmentsService.deleteAssignmentsById(uuid)?
                 ResponseEntity.status(204).build() : ResponseEntity.status(404).build();
     }
@@ -79,9 +89,11 @@ public class AssignmentsController {
         assignments.setPoints(jsonNode.get("points").intValue());
         assignments.setAssignmentUpdated(LocalDateTime.now());
         if (!assignmentsService.updateAssignmnentsById(uuid, assignments)){
+            statsd.incrementCounter("api.assignments.update.failed");
             return ResponseEntity.status(404).build();
         }
         log.info("Assignment Updated in Database");
+        statsd.incrementCounter("api.assignments.update");
         return ResponseEntity.status(204).build();
 
     }
