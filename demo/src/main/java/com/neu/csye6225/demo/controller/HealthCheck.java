3d79e51573657db0b8e7c45968d64b9b9168be67
Vedantini Gaikwad <114186433+VedantiniG@users.noreply.github.com>
2023-11-07 22:49:00 -0500

    Configure statsd for logging metrics

--- demo/src/main/java/com/neu/csye6225/demo/controller/HealthCheck.java
+++ demo/src/main/java/com/neu/csye6225/demo/controller/HealthCheck.java
@@ -1,6 +1,7 @@
 package com.neu.csye6225.demo.controller;
 
 import com.neu.csye6225.demo.service.HealthCheckService;
+import com.timgroup.statsd.StatsDClient;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,9 @@ public class HealthCheck {
     @Autowired
     private final HikariDataSource hikariDataSource;
 
+    @Autowired
+    StatsDClient statsDClient;
+
     public HealthCheck (HealthCheckService healthCheckService, HikariDataSource hikariDataSource) {
         this.healthCheckService = healthCheckService;
         this.hikariDataSource = hikariDataSource;
@@ -23,10 +27,13 @@ public class HealthCheck {
     public ResponseEntity<String> getHealthCheck() {
         try {
             if (healthCheckService.dbConnectionCheck()) {
+                statsDClient.increment("api.healthCheck.ok");
                 return ResponseEntity.ok().build();
             }
+            statsDClient.increment("api.healthCheck.failed");
             return ResponseEntity.status(503).build();
         } catch (Exception exception) {
+            statsDClient.increment("api.healthCheck.failed");
             return ResponseEntity.status(503).build();
         }
     }
